2e90eadd96300d992f63ffc1943af36eb9d23f42
TheAlexGo <alex280702@mail.ru>
2022-04-15 00:40:47 +0300

    Добавьте файлы проекта.

---
+++ Pages/CartPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using AromatniyMir.Models;
+
+namespace AromatniyMir.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CartPage.xaml
+    /// </summary>
+    public partial class CartPage : Page
+    {
+        public int quantityInCart { get; set; }
+        public decimal costWithQuantity { get; set; }
+        public OrderProduct? orderProduct { get; set; }
+
+        public CartPage(List<Product> products)
+        {
+            InitializeComponent();
+            dataGridProducts.ItemsSource = products.OrderBy(p => p.ProductName).Distinct();
+            foreach (var p in products.OrderBy(p => p.ProductName).Distinct())
+            {
+                orderProduct = new OrderProduct
+                {
+                    ProductArticle = p.ProductArticleNumber,
+                    ProductAmount = products.Where(e => e == p).Count()
+                };
+            }
+        }
+
+        private void buttonQuantityPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonQuantityMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Pages/OrdersPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AromatniyMir.Models;
+
+namespace AromatniyMir.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для OrdersPage.xaml
+    /// </summary>
+    public partial class OrdersPage : Page
+    {
+        public OrdersPage()
+        {
+            InitializeComponent();
+            if (App.CurrentUser?.UserRole == 3)
+                btnShowProductsInOrder.Visibility = Visibility.Collapsed;
+            List<Order> ordersList = App.db.Orders.ToList();
+            dataGridOrders.ItemsSource = ordersList;
+        }
+
+        private void btnShowProductsInOrder_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGridOrders.SelectedIndex == -1)
+                MessageBox.Show("Выберите заказ, который хотите просмотреть", "Ошибка при просмотре заказа",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            else
+            {
+                var row = dataGridOrders.SelectedItem as Order;
+                if (row != null)
+                {
+                    int result = row.OrderId;
+                    NavigationService.Navigate(new OrderProductListPage(result));
+                }
+            }
+        }
+    }
+}
